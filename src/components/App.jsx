9f540428f19fdee6212f849b67cf776f55620d2a
Carlos Pretelin <carlosppretelin@gmail.com>
2022-12-05 09:39:06 -0600

    pic displayed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
+
+
+
+    const API = "https://api.thecatapi.com/v1/images/search";
+    const [foto, setFoto] = useState("")
+
+    useEffect(()=>{
+        fetch(API)
+        .then(res => res.json())
+        .then(data => {
+            setFoto(data[0].url)
+        })
+    }, [])
   return (
-    <div>hola mundo</div>
+    <div>
+        <img src={foto} alt="" />
+    </div>
   )
 }
 
