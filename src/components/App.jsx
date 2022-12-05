0206daeb82b2197416a9ba49d37964cc2033f5f2
Carlos Pretelin <carlosppretelin@gmail.com>
2022-12-05 10:55:17 -0600

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,40 @@ const App = () => {
 
 
 
-    const API = "https://api.thecatapi.com/v1/images/search";
+    const API = "https://api.thecatapi.com/v1/images/search?limit=3";
     const [foto, setFoto] = useState("")
+    const [foto2, setFoto2] = useState("")
+    const [foto3, setFoto3] = useState("")
 
     useEffect(()=>{
         fetch(API)
         .then(res => res.json())
         .then(data => {
             setFoto(data[0].url)
+            setFoto2(data[1].url)
+            setFoto3(data[2].url)
         })
     }, [])
+
+
+    const getPic = () =>{
+        fetch(API)
+        .then(res => res.json())
+        .then(data => {
+            setFoto(data[0].url)
+            setFoto2(data[1].url)
+            setFoto3(data[2].url)
+        })
+    }
+
+
   return (
     <div>
-        <img src={foto} alt="" />
+        <img width={150} src={foto} alt="" />
+        <img width={150} src={foto2} alt="" />
+        <img width={150} src={foto3} alt="" />
+        <button onClick={getPic}
+        >Change Pic</button>
     </div>
   )
 }
